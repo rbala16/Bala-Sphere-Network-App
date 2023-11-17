24f55d36799c87d78c670e5b854f9f0a373842a2
Rajni bala <balatamna16@gmail.com>
2023-11-16 20:22:24 -0500

    pages and components file created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-
+import { Outlet,Navigate,Route,Routes,useLocation } from 'react-router-dom';
 import './App.css';
+import{Home,Profile,Register,Login,ResetPassword} from './pages/index';
 
+function Layout(){
+const user = null;
+const location = useLocation()
+
+return user?.token?(
+  <Outlet/>
+): (
+  <Navigate to = "/login" state={{from:location}} replace />
+);
+}
 function App() {
   return (
     <div className="App">
-     <p>World</p>
+     <Routes>
+      <Route element={<Layout/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile/:id?' element={<Profile/>}/>
+      </Route>
+      <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      
+      
+     </Routes>
     </div>
   );
 }
